9696657bce3db4932503e57416bc7b34b457153e
chencheng <chencheng@juloloo.com>
2015-12-23 23:17:34 +0800

    grunt integration

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt){
+'use strict';
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		dirs: {
+			qinaka:'./app/qianka'
+			itry: './app/itry',
+			comob: './app/comob'
+		},
+
+		uglify: {	
+			options: {
+				stripBanners : true,
+				banner : '/* !<%=pkg.name%> - <%=pkg.version%>.js  <%= grunt.template.today("yyyy mm dd")%>'
+			},
+			build: {
+				src: 'src/content-script.js',
+				dest: 'build/<%=pkg.name%>-<%=pkg.version%>.js.min.js'
+			}
+		},
+		jshint: {
+			testsx: ['Gruntfile.js','src/*.js'],
+			options: {
+				jshintrc: '.jshintrc'
+			}
+		},
+		watch: {
+			build: {
+				files: ['src/js/*.js'],
+				tasks: ['jshint','uglify'],
+				options: {spwan: false}
+			}
+		},
+		concat: {
+			common: {
+				files: {
+					src:
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+	grunt.registerTask('default',['jshint','uglify','watch']);
+};
---
+++ src/js/common/music.js
@@ -0,0 +1,62 @@
+define("common/music",[],function(require,exports){
+
+	function message(){
+ 		chrome.runtime.sendMessage({type:"triggerMusic",switcher:"on"},function(response){
+
+		 	console.log(response); 
+		
+		});
+ 	}
+
+ 	function email(){
+
+	    var xhr = new XMLHttpRequest();
+
+        xhr.open("POST","http://localhost:18080/apptry");
+        xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+
+        xhr.onreadystatechange = function(){
+        	if(xhr.readyState == 4){
+        		var resp = ("(" + xhr.responseText + ")")
+        		console.log(resp);
+        	}
+        }
+
+    	xhr.send("pname=" + tryerUse.pname);
+ 	}
+
+ 	exports.sendEmail = email;
+
+ 	exports.sendMusic = message;
+
+	exports.musicAndEmail = function(ls){
+
+		var cirleInterval = +localStorage.hasTask;
+
+		console.log("ls------"+ls);
+
+		console.log("cirleInterval-------"+cirleInterval);
+
+		if (ls > 0){
+		        // alert("有任务");
+
+		        //判断是否已做过提醒
+		        if( cirleInterval == 0){
+
+		        	localStorage.hasTask = 1;
+
+		         	//触发背景音乐
+		         	
+		         	setTimeout(message,0);
+
+				}
+		 }else{
+		//         location.reload();
+				if(cirleInterval == 1 ){//重置提醒记录
+
+					localStorage.hasTask = 0;
+
+				}
+		 }
+	}
+});
