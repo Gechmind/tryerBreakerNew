e5c67c8f466e286bae0180891efc9a7399d40d5d
chencheng <chencheng@juloloo.com>
2016-01-02 11:22:36 +0800

    build

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,51 +4,37 @@ module.exports = function(grunt){
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
-		dirs: {
-			qinaka:'./app/qianka'
-			itry: './app/itry',
-			comob: './app/comob'
-		},
-
 		uglify: {	
-			options: {
-				stripBanners : true,
-				banner : '/* !<%=pkg.name%> - <%=pkg.version%>.js  <%= grunt.template.today("yyyy mm dd")%>'
+			dynamic_mappings: {
+      		// Grunt will search for "**/*.js" under "lib/" when the "uglify" task
+      		// runs and build the appropriate src-dest file mappings then, so you
+      		// don't need to update the Gruntfile when files are added or removed.
+		      files: [
+		        {
+		          expand: true,     // Enable dynamic expansion.
+		          cwd: 'src/',      // Src matches are relative to this path.
+		          src: ['**/*.js'], // Actual pattern(s) to match.
+		          dest: 'build/',   // Destination path prefix.
+		          ext: '.min.js',   // Dest filepaths will have this extension.
+		          extDot: 'first'   // Extensions in filenames begin after the first dot
+		        },
+		      ],
+		    }
+		},
+		clean: {
+			dist:{
+				src:['build/llls.js']
 			},
-			build: {
-				src: 'src/content-script.js',
-				dest: 'build/<%=pkg.name%>-<%=pkg.version%>.js.min.js'
-			}
-		},
-		jshint: {
-			testsx: ['Gruntfile.js','src/*.js'],
-			options: {
-				jshintrc: '.jshintrc'
-			}
-		},
-		watch: {
-			build: {
-				files: ['src/js/*.js'],
-				tasks: ['jshint','uglify'],
-				options: {spwan: false}
-			}
-		},
-		transport: {
-
-		},
-		concat: {
-			common: {
-				files: {
-					src:
-				}
+			options:{
+				'no-write':true
 			}
 		}
+
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-clean');
 
-
-	grunt.registerTask('default',['jshint','uglify','watch']);
+	grunt.registerTask('default',['uglify']);
+	grunt.registerTask('clean',['clean']);
 };
